65f038dbc9b757140d906386b1e1025c2054dd47
carlos <chlouzada@gmail.com>
2022-09-11 19:54:38 -0300

    add nativewind

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useQuery } from "@tanstack/react-query";
 import { search, story, topStories } from "../queries/hn";
+import { styled } from "nativewind";
+
+const StyledText = styled(Text);
 
 export default function Index() {
   const topStoriesQuery = useQuery(["topStories"], topStories);
@@ -9,18 +12,18 @@ export default function Index() {
   const searchQuery = useQuery(["search"], () => search({ query: "react" }));
 
   return (
-    <View style={styles.container}>
+    <View  style={styles.container}>
+      <StyledText className="bg-red-900 p-5">Ola</StyledText>
       {storyQuery.data && <Text>{storyQuery.data.title}</Text>}
 
       <View style={{ height: 60 }} />
-      {searchQuery.data?.hits?.slice(0,5).map((el) => (
+      {searchQuery.data?.hits?.slice(0, 5).map((el) => (
         <Text key={el.objectID}>{el.title}</Text>
       ))}
 
       <View style={{ height: 60 }} />
 
-
-      {topStoriesQuery.data?.slice(0,5).map((el) => (
+      {topStoriesQuery.data?.slice(0, 5).map((el) => (
         <Text key={el.id}>{el.title}</Text>
       ))}
     </View>

df909a452ff4b80b93d05a18a7c1e595057d2fea
carlos <chlouzada@gmail.com>
2022-09-11 23:01:13 -0300

    top stories list

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,40 +1,81 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { useQuery } from "@tanstack/react-query";
-import { search, story, topStories } from "../queries/hn";
+import { topStories } from "../queries/hn";
 import { styled } from "nativewind";
+import { date } from "../utils/date";
+import { useFonts } from "expo-font";
 
+const StyledScrollView = styled(ScrollView);
 const StyledText = styled(Text);
+const StyledView = styled(View);
 
-export default function Index() {
-  const topStoriesQuery = useQuery(["topStories"], topStories);
-  const storyQuery = useQuery(["story"], () => story(5218288));
-  const searchQuery = useQuery(["search"], () => search({ query: "react" }));
+const StoryItem = ({
+  id,
+  title,
+  score,
+  author,
+  createdAt,
+  comments,
+}: {
+  id: number;
+  title: string;
+  score: number;
+  author: string;
+  comments: number;
+  createdAt: string;
+}) => {
+  return (
+    <StyledView key={id} className="p-2">
+      {/* <Link href={`/story/${id}`}> */}
+      <StyledView>
+        <StyledText className="text-white font-bold text-lg">
+          {title}
+        </StyledText>
+        <StyledText
+          className="text-xs text-white"
+        >
+          {author} ({score})
+        </StyledText>
+        <StyledView className="flex flex-row justify-between text-sm font-bold">
+          <StyledText className="text-white">{comments} comments</StyledText>
+          <StyledText className="text-white">{date(createdAt)}</StyledText>
+        </StyledView>
+      </StyledView>
+      {/* </Link> */}
+    </StyledView>
+  );
+};
+
+const StoriesView = () => {
+  const { data, isLoading, isError } = useQuery(["topStories"], topStories);
+
+  if (isLoading) return <StyledText>Loading...</StyledText>;
+  if (isError) return <StyledText>Error</StyledText>;
 
   return (
-    <View  style={styles.container}>
-      <StyledText className="bg-red-900 p-5">Ola</StyledText>
-      {storyQuery.data && <Text>{storyQuery.data.title}</Text>}
+    <StyledView>
+      <StyledView>
+        {data.map((story) => (
+          <StoryItem
+            key={story.id}
+            id={story.id}
+            title={story.title}
+            score={story.score}
+            author={story.by}
+            comments={story.descendants}
+            createdAt={new Date(story.time * 1000).toISOString()}
+          />
+        ))}
+      </StyledView>
+    </StyledView>
+  );
+};
 
-      <View style={{ height: 60 }} />
-      {searchQuery.data?.hits?.slice(0, 5).map((el) => (
-        <Text key={el.objectID}>{el.title}</Text>
-      ))}
-
-      <View style={{ height: 60 }} />
-
-      {topStoriesQuery.data?.slice(0, 5).map((el) => (
-        <Text key={el.id}>{el.title}</Text>
-      ))}
-    </View>
+export default function Index() {
+  return (
+    <StyledScrollView className="bg-black">
+      <StoriesView />
+    </StyledScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

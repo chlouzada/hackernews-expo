28acdb29ac523f40e683cab0d469fbac20409014
carlos <chlouzada@gmail.com>
2022-09-14 18:20:28 -0300

    fix

--- src/views/LoadingView.tsx
+++ src/views/LoadingView.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { StyleProp, Text, TextStyle, View } from "react-native";
+import { Dimensions, Text, View } from "react-native";
 
 export default function LoadingView() {
+  const height = Dimensions.get("window").height;
   return (
     <View
       style={{
         backgroundColor: "black",
-        height: "100vh",
+        height: height,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
